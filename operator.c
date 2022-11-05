056412772595eca8c76c5a3ae04cb7be004028d5
jorgejcdev <110706936+jorgejcdev@users.noreply.github.com>
2022-11-05 01:58:21 -0300

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(void)
+{
+    printf("the type variables int use %d bytes\n", sizeof(int));
+    printf("the type variables float use %d bytes\n", sizeof(float));
+    printf("the type variables double use %d bytes\n", sizeof(double));
+    printf("the type variables unsigned use %d bytes\n", sizeof(unsigned));
+    printf("the type variables long use %d bytes\n", sizeof(long));
+    
+}
